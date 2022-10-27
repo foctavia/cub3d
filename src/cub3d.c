1e710f5e9d7a2d047d8cade4410fc037147a2fb0
owalsh <owalsh@student.42.fr>
2022-10-27 09:31:17 +0200

    Add condition in main to test discord webhook
    *

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: owalsh <owalsh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 09:01:50 by owalsh            #+#    #+#             */
-/*   Updated: 2022/10/27 09:19:46 by owalsh           ###   ########.fr       */
+/*   Updated: 2022/10/27 09:31:11 by owalsh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
+	if (argc == 1)
+		printf("program requires map as argument.\n");
 	(void)argv;
 	printf("coucou\n");
 	return (EXIT_SUCCESS);
